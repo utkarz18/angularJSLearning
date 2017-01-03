a73f9a112ba399427787210b16a12440008ba97f
Utkarsh <utkarz18@gmail.com>
2017-01-04 01:06:12 +0530

    Test

--- dthTest/app.js
+++ dthTest/app.js
@@ -8,16 +8,15 @@
 	function DthController(DthService){
 		var dth = this;
 		dth.client_id = "10356";
-		dth.client_key = "e1k97j1plhgdrqc0b8olannbxnnilcnf";
-		dth.custid = "";
+		dth.client_key = "rfuzoqsr0u8v36glg8wn11c5gbs6gtfe";
+		dth.msisdn = "";
 		dth.opCode = "";
 		dth.amount = "";
 		dth.live = true;
 		dth.message = "";
 		
 		dth.fillCharge = function(){
-			console.log(dth.client_id, dth.client_key, dth.custid, dth.opCode, dth.amount, dth.live);
-			var promise = DthService.fillCharge(dth.client_id, dth.client_key, dth.custid, dth.opCode, dth.amount, dth.live);
+			var promise = DthService.fillCharge(dth.client_id, dth.client_key, dth.msisdn, dth.opCode, dth.amount, dth.live);
 			promise.then(function(response){
 				console.log(response)
 				if(response == "Error")
@@ -29,9 +28,15 @@
 	DthService.$inject = ['$http', 'ApiPath'];
 	function DthService($http, ApiPath){
 		var service = this;
-		service.fillCharge = function(client_id, client_key, custid, opCode, amount, live){
-			return $http.get(ApiPath + 'client_id=' + client_id + '&client_key=' + client_key + '&custid=' + custid 
-							 + '&operator_code=' + opCode + '&amount=' + amount + '&live=' + live)
+		service.fillCharge = function(client_id, client_key, msisdn, opCode, amount, live){
+			var config = {
+				 headers: {
+					'Content-Type': 'application/json', 
+					'Accept': 'application/json'
+				}
+			}
+			return $http.post(ApiPath + 'client_id=' + client_id + '&client_key=' + client_key + '&msisdn=' + msisdn 
+					                 + '&operator_code=' + opCode + '&amount=' + amount + '&live=' + live, config)
 			.then(function (response) {
 				return response.data;
 			})
